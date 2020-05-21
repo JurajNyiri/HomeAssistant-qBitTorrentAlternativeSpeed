232f4e5dd82e27e132fdf120443e28e5251d5f93
Juraj Nyiri <jnyiri@sk.ibm.com>
2020-05-21 12:55:55 +0200

    Update: Support for Home Assistant 0.110.0

--- custom_components/qbittorrent_alternative_speed/switch.py
+++ custom_components/qbittorrent_alternative_speed/switch.py
@@ -1,12 +1,20 @@
-__version__ = "1.3"
+__version__ = "1.3.1"
 
 import logging
 import voluptuous as vol
 
 from qbittorrentapi import Client
 
-from homeassistant.components.switch import (SwitchDevice, PLATFORM_SCHEMA)
-from homeassistant.const import (CONF_PROTOCOL,CONF_NAME,CONF_PORT, CONF_HOST,CONF_USERNAME,CONF_PASSWORD)
+try:
+    from homeassistant.components.switch import (
+        SwitchEntity, PLATFORM_SCHEMA)
+except ImportError:
+    from homeassistant.components.switch import (
+        SwitchDevice as SwitchEntity, PLATFORM_SCHEMA)
+
+
+from homeassistant.const import (
+    CONF_PROTOCOL, CONF_NAME, CONF_PORT, CONF_HOST, CONF_USERNAME, CONF_PASSWORD)
 import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
@@ -20,23 +28,27 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default="qbittorrent_alternative_speed"): cv.string
 })
 
+
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    host = config.get(CONF_PROTOCOL)+"://"+config.get(CONF_HOST)+":"+config.get(CONF_PORT)
+    host = config.get(CONF_PROTOCOL)+"://" + \
+        config.get(CONF_HOST)+":"+config.get(CONF_PORT)
     username = config.get(CONF_USERNAME)
     password = config.get(CONF_PASSWORD)
     name = config.get(CONF_NAME)
 
-    add_devices([qbittorrent_alternative_speed(host, username, password, name)], True)
+    add_devices([qbittorrent_alternative_speed(
+        host, username, password, name)], True)
 
 
-class qbittorrent_alternative_speed(SwitchDevice):
+class qbittorrent_alternative_speed(SwitchEntity):
 
     def __init__(self, host, username, password, name):
         self.host = host
         self.username = username
         self.password = password
         self._name = name
-        self.qb = Client(host=self.host, username=self.username, password=self.password)
+        self.qb = Client(host=self.host, username=self.username,
+                         password=self.password)
         self.signin()
 
     def signin(self):
